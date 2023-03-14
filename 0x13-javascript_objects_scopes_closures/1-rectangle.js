d8b504cc8d76357117819c3d7a347dafec63ece6
Warsame Jama <warsame.jama80@gmail.com>
2023-03-14 01:09:15 -0700

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+<<<<<<< HEAD
+// JS Script
+=======
+>>>>>>> refs/remotes/origin/main
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
